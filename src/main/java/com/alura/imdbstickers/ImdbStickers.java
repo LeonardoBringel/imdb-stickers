dd95a8c2e93f1044b4ef7cf0c751a850ab554a27
LeonardoBringel <leo.sbringel@gmail.com>
2022-07-20 21:16:06 -0300

    :sparkles: Implement sticker generator

--- src/main/java/com/alura/imdbstickers/ImdbStickers.java
+++ src/main/java/com/alura/imdbstickers/ImdbStickers.java
@@ -2,12 +2,16 @@ package com.alura.imdbstickers;
 
 import com.alura.controller.ImdbApi;
 import com.alura.model.Movie;
+import com.alura.model.Sticker;
 import java.util.List;
 
 public class ImdbStickers {
 
     public static void main(String[] args) {
         List<Movie> movies = ImdbApi.FetchMovies();
-        movies.forEach((var movie) -> System.out.println(movie.getTitle() + " (" + movie.getYear() + ")"));
+        movies.forEach((var movie) -> System.out.println(movie.getTitle() + " (" + movie.getYear() + ")" + " - " + movie.getImage()));
+
+        var walleUrl = "https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@.jpg";
+        new Sticker(walleUrl).generate();
     }
 }
---
+++ src/main/java/com/alura/model/Sticker.java
@@ -0,0 +1,32 @@
+package com.alura.model;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class Sticker {
+
+    public final String URL;
+
+    private BufferedImage image;
+
+    public Sticker(String url) {
+        this.URL = url;
+    }
+
+    public void generate() {
+        try {
+            var inputStream = new URL(URL).openStream();
+            this.image = ImageIO.read(inputStream);
+
+            var file = new File("stickers/sticker.png");
+            ImageIO.write(this.image, "png", file);
+
+        } catch (IOException ex) {
+            System.out.println("Oops! " + ex.getMessage());
+            System.exit(0);
+        }
+    }
+}
